566c8e8411e5996833941dd47c46d968f0f4ff3d
wizzzpy <70415783+Wisp369@users.noreply.github.com>
2026-04-22 15:14:45 -0400

    Initial commit

---
+++ Backend/src/main/java/ca/wisp/studycorner/StudycornerApplication.java
@@ -0,0 +1,13 @@
+package ca.wisp.studycorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudycornerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/ca/wisp/studycorner/StudycornerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.wisp.studycorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudycornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
