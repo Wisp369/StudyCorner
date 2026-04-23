39b83bb4a3a5e9fb11f4ad6e09bf43b38a453eb4
wizzzpy <70415783+Wisp369@users.noreply.github.com>
2026-04-23 11:56:21 -0400

    Added BackendController with a basic GET mapping for getting all courses

---
+++ Backend/src/main/java/ca/wisp/studycorner/controller/BackendController.java
@@ -0,0 +1,25 @@
+package ca.wisp.studycorner.controller;
+
+import ca.wisp.studycorner.model.Course;
+import ca.wisp.studycorner.service.CourseService;
+import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+@CrossOrigin("http://localhost:5173")
+class BackendController {
+  private CourseService _courseService;
+
+  public BackendController() {
+    _courseService = new CourseService();
+  }
+
+  @GetMapping("/courses")
+  public List<Course> getCourses() {
+    return _courseService.getCourses();
+  }
+}
