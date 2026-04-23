7e8170505b33a8499789943d14fbf0d35ecf03d8
wizzzpy <70415783+Wisp369@users.noreply.github.com>
2026-04-23 11:00:47 -0400

    Added the course class to the backend

---
+++ Backend/src/main/java/ca/wisp/studycorner/model/Course.java
@@ -0,0 +1,54 @@
+package ca.wisp.studycorner.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer courseId;
+
+  private String courseName;
+  private String courseCode;
+  private String professorName;
+
+  public Course(String courseName) {
+    this.courseName = courseName;
+  }
+
+  public Integer getCourseId() {
+    return courseId;
+  }
+
+  public void setCourseId(Integer courseId) {
+    this.courseId = courseId;
+  }
+
+  public String getCourseName() {
+    return courseName;
+  }
+
+  public void setCourseName(String courseName) {
+    this.courseName = courseName;
+  }
+
+  public String getCourseCode() {
+    return courseCode;
+  }
+
+  public void setCourseCode(String courseCode) {
+    this.courseCode = courseCode;
+  }
+
+  public String getProfessorName() {
+    return professorName;
+  }
+
+  public void setProfessorName(String professorName) {
+    this.professorName = professorName;
+  }
+}
